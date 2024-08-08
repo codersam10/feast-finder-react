b2df27174e3671646930295385f69228de01a6a6
sam <savantsamarth@gmail.com>
2024-08-08 19:54:08 +0530

    style- style shimmer component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,20 +1,21 @@
-
-import { useRouteError} from "react-router-dom"
+import { useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
 const Error = () => {
   const err = useRouteError();
-  const style ={
-    display:"flex",
-    justifyContent: "center",
-    alignItems:"center",
-    height:"100vh",
-    flexDirection:"column",
-    color:"red",
-    fontSize:"2rem"
-  }
-  console.log(err)
+
   return (
-    <div className="error" style={style}>
-      <h1>Error {err.status} {err.statusText}</h1>
+    <div className="h-screen bg-slate-100 flex justify-center items-center">
+      <div class="text-center border-2 border-red-600 p-4 rounded-lg">
+        <h1 className="p-2 text-red-600 text-5xl font-semibold mb-2">
+          Error {err.status} {err.statusText}
+        </h1>
+
+        <Link to="/">
+          <button className="p-2 rounded-md bg-slate-200 text-red-600 hover:shadow-md">
+            Go to Home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,101 +1,100 @@
 const Shimmer = () => {
-
   return (
-    <div className="shimmer-container">
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+    <div className="shimmer-container flex flex-wrap gap-5 justify-center py-10">
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
 
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
 
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
 
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
 
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
 
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
 
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
 
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
 
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
 
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
 
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
 
-      <div className="shimmer-card">
-        <div className="shimmer-img"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
-        <div className="shimmer-info"></div>
+      <div className="shimmer-card res-card w-72 rounded-xl shadow-md bg-slate-200 p-4">
+        <div className="shimmer-img bg-slate-300 mb-2 w-full h-32 rounded-md"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
+        <div className="shimmer-info mb-3 bg-slate-300 rounded-full h-2"></div>
       </div>
     </div>
   );
