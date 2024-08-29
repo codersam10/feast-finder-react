73e616b3856d4d9eec2b657918c5a92b9e4d0ab8
sam <savantsamarth@gmail.com>
2024-08-29 15:41:40 +0530

    refactor- use .env file instead of hard coding firebase api key

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Add SDKs for Firebase products from here if required
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDVGcCbkHB91vuZ-6QWSBqK8XEEwdp8Jfs",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "feast-finder-react.firebaseapp.com",
   projectId: "feast-finder-react",
   storageBucket: "feast-finder-react.appspot.com",
   messagingSenderId: "489613586211",
-  appId: "1:489613586211:web:be293ebe8d3b67b14ce696",
-  measurementId: "G-C17GM6FQK1",
+  appId: "1:489613586211:web:4c46b0adee3b3b474ce696",
+  measurementId: "G-H19BRG7TX4"
 };
 
 // Initialize Firebase and get a reference to the Firebase app object
