ad6b0857123baaf44a6b2f201225f41aa6774370
sam <savantsamarth@gmail.com>
2025-02-02 20:34:19 +0530

    refactor- add cleanup function for useOnlineStatus

--- src/utilis/useOnlineStatus.js
+++ src/utilis/useOnlineStatus.js
@@ -2,9 +2,15 @@ import { useState, useEffect } from "react";
 
 const useOnlineStatus = () => {
   const [onlineStatus, setOnlineStatus] = useState(true);
+
   useEffect(() => {
     window.addEventListener("offline", () => setOnlineStatus(false));
     window.addEventListener("online", () => setOnlineStatus(true));
+
+    return () => {
+      window.removeEventListener("offline", () => setOnlineStatus(false));
+      window.removeEventListener("online", () => setOnlineStatus(true));
+    };
   }, []);
 
   return onlineStatus;
